3cb895d4cc33bdfc4a44812f474d07f058022481
Jfranbm04 <juanfranburgos2004@gmail.com>
2026-02-06 08:45:24 +0100

    CREATE: Project

---
+++ Assets/Scripts/FaceRunnerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class FaceRunnerController : MonoBehaviour
+{
+    public ARFace face;
+    public Transform player;
+
+    public float lateralSensitivity = 0.1f;
+    public float maxX = 2f;
+
+    void Update()
+    {
+
+        if (face == null || player == null)
+            return;
+
+        Vector3 headRotation = face.transform.localEulerAngles;
+
+        float yaw = headRotation.y;
+        if (yaw > 180) yaw -= 360;
+
+        float targetX = -yaw * lateralSensitivity;
+        targetX = Mathf.Clamp(targetX, -maxX, maxX);
+
+        Vector3 pos = player.position;
+        pos.x = Mathf.Lerp(pos.x, targetX, Time.deltaTime * 5f);
+        player.position = pos;
+
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if(face == null || player == null)
+    //        return;
+    //    Vector3 headRotation = face.transform.localEulerAngles;
+
+    //    float giroCabeza = headRotation.y;
+    //    if(giroCabeza > 180) giroCabeza -= 360;
+
+
+
+    //    player.Translate(new Vector3(giroCabeza * Time.deltaTime, 0, 0));
+
+    //}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int vidas = 3;
+    public int puntuacion = 20;
+    public bool isGrounded;
+    public TextMeshProUGUI LivesText;
+    public TextMeshProUGUI ScoreText;
+    public GameObject LosePanel;
+    public GameObject WinPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LivesText.text = "Vidas: " + vidas;
+        ScoreText.text = "Cangreburgers restantes: " + puntuacion;
+        LosePanel.SetActive(false);
+        WinPanel.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            vidas--;
+            LivesText.text = "Vidas: " + vidas;
+            if (vidas <= 0)
+            {
+                Time.timeScale = 0;
+                LosePanel.SetActive(true);
+            }
+        }
+
+        if (other.CompareTag("Collectible"))
+        {
+            puntuacion -= 1;
+            ScoreText.text = "Cangreburgers restantes: " + puntuacion;
+
+            if (puntuacion < 1)
+            {
+                Time.timeScale = 0;
+                WinPanel.SetActive(true);
+            }
+        }
+    }
+
+
+
+    public void ReiniciarJuego()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void VolverMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+}
